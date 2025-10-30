318211e7ff50af6c6bf526b4c61e01888c99aaea
badrneymar2010-cloud <badrneymar2010@alumnes.ilerna.com>
2025-10-30 19:30:13 +0100

    modificacio del codi per el percentatge.

--- Calculadora/src/Calculadora/calculadora.java
+++ Calculadora/src/Calculadora/calculadora.java
@@ -1,17 +1,26 @@
 package Calculadora;
 
-public class calculadora {
+import java.util.Scanner;
 
-	public class Calculadora {
+
+	public class calculadora {
 	    public static void main(String[] args) {
+	    	Scanner sc = new Scanner(System.in);
+	    	
+	    	double a;
+	    	double b;
+
 	        System.out.println("=== Calculadora Básica ===");
-	        int a = 10;
-	        int b = 5;
+	        System.out.println("Valor a ");
+			a = sc.nextDouble();
+	        System.out.println("Valor b ");
+			b = sc.nextDouble();
+
 	 
 	        System.out.println("Suma: " + (a + b));
 	        System.out.println("Resta: " + (a - b));
 	        System.out.println("Multiplicación: " + (a * b));
 	        System.out.println("División: " + (a / b));
+	        System.out.println("Percentatge: " + ((a / b) * 100) + "%");
 	    }
 	}
-}
